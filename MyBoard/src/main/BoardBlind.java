e4319d88881e3692316bbe02c6773a744328c57a
seungjae <juaje03@naver.com>
2024-03-28 18:14:41 +0900

    Create: BoardBlind, BoardNonBlind class + add Constructor Method

---
+++ MyBoard/src/main/BoardBlind.java
@@ -0,0 +1,13 @@
+package main;
+
+
+import dto.Board;
+
+public class BoardBlind extends Board {
+
+	public BoardBlind(String id, String content, String title) {
+		super(id, content, title);
+	}
+
+	
+}
---
+++ MyBoard/src/main/BoardNonBlind.java
@@ -0,0 +1,11 @@
+package main;
+
+import dto.Board;
+
+public class BoardNonBlind extends Board {
+
+	public BoardNonBlind(String content, String title) {
+		super(content, title);
+	}
+
+}
