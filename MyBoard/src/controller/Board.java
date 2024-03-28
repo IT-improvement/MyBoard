82575066b43f2d3fde7ccdac4260f84b5d723788
seungjae <juaje03@naver.com>
2024-03-28 18:03:59 +0900

    Modify: edit date Type, Constructor + add SimpleDateFormat variable (in Board abstract class)

--- MyBoard/src/controller/Board.java
+++ MyBoard/src/controller/Board.java
@@ -1,7 +1,59 @@
 package controller;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public abstract class Board {
-	
+	private String id;
+	private String content;
+	private String title;
+	private String date;
+	private SimpleDateFormat sdf;
+
+	public Board(String id, String content, String title) {
+		sdf = new SimpleDateFormat("yy년 mm월 dd일 kk:mm");
+		this.id = id;
+		this.content = content;
+		this.title = title;
+		this.date = sdf.format(System.currentTimeMillis());
+	}
+
+	public Board(String content, String title) {
+		sdf = new SimpleDateFormat("yy년 mm월 dd일 kk:mm");
+		this.content = content;
+		this.title = title;
+		this.date = sdf.format(System.currentTimeMillis());
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 }
