b58d276245ec1a4fd03919ea9a5b9774b1788b1d
seungjae <juaje03@naver.com>
2024-03-29 16:12:00 +0900

    Delete: SimpleDateFormat variable in BoardManager Class

--- MyBoard/src/controller/BoardManager.java
+++ MyBoard/src/controller/BoardManager.java
@@ -1,15 +1,18 @@
 package controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import dto.Board;
+import unit.BoardNonBlind;
 
 public class BoardManager {
 	// static ArrayList<Board>
-	public static ArrayList<Board> boardList = new ArrayList<>();
+	public static ArrayList<BoardNonBlind> boardList = new ArrayList<>();
+	private SimpleDateFormat sdf;
 
 	private BoardManager() {
-
+		sdf = new SimpleDateFormat("yy년 mm월 dd일 kk:mm");
 	}
 
 	private static BoardManager instance = new BoardManager();
@@ -17,4 +20,11 @@ public class BoardManager {
 	public static BoardManager getInsatnce() {
 		return instance;
 	}
+
+	/* C(insert) */
+	public void addBoard(String id, String title, String content) {
+		String date = sdf.format(System.currentTimeMillis());
+		BoardNonBlind board = new BoardNonBlind(id, title, content, date);
+		boardList.add(board);
+	};
 }
