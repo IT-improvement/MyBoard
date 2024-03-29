dca700e0e7e2eaf123982c061fe91451983fa455
seungjae <juaje03@naver.com>
2024-03-29 10:55:25 +0900

    Add: ArrayList Variable in UserManager Class

--- MyBoard/src/controller/UserManager.java
+++ MyBoard/src/controller/UserManager.java
@@ -1,10 +1,15 @@
 package controller;
 
+import java.util.ArrayList;
+
+import unit.User;
+
 public class UserManager {
 	private UserManager() {
 
 	}
 
+	private static ArrayList<User> userList = new ArrayList<>();
 	private static UserManager instance = new UserManager();
 
 	public static UserManager getInstance() {
