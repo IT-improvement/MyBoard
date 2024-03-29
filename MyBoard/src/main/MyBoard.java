6e01bbb45e7e9b8bbcef7fe42ddff11d12859a96
seungjae <juaje03@naver.com>
2024-03-29 10:13:17 +0900

    Add: Variable HashMap, HashSet + Constructor + run, printStart Method in MyBoard Class

--- MyBoard/src/main/MyBoard.java
+++ MyBoard/src/main/MyBoard.java
@@ -1,5 +1,28 @@
 package main;
 
+import java.util.*;
+
+import unit.BoardBlind;
+import unit.BoardNonBlind;
+import unit.User;
+
 public class MyBoard {
 
+	private Map<User, ArrayList<BoardNonBlind>> list = new HashMap<>();
+	private Set<BoardBlind> unknownList = new HashSet<>();
+	
+	
+	public MyBoard() {
+		
+	}
+	
+	public void run() {
+
+	}
+
+	/* print */
+	private void printStart() {
+		System.out.println("1)회원가입");
+		System.out.println("2)로그인");
+	}
 }
