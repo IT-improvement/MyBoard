133f1b1c14dbf5a9e987d7d7fdb26564ee254547
seungjae <juaje03@naver.com>
2024-03-29 09:14:55 +0900

    Add: implements Updateable mark interface in BoardNonBlind

--- MyBoard/src/unit/BoardNonBlind.java
+++ MyBoard/src/unit/BoardNonBlind.java
@@ -1,8 +1,9 @@
 package unit;
 
 import dto.Board;
+import model.Updateable;
 
-public class BoardNonBlind extends Board {
+public class BoardNonBlind extends Board implements Updateable {
 
 	public BoardNonBlind(String content, String title) {
 		super(content, title);
