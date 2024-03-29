65d7d87d910a0f11f429fd06d7f059ba54567cba
seungjae <juaje03@naver.com>
2024-03-29 10:15:30 +0900

    Add: printAdminMenu Method in MyBoard Class

--- MyBoard/src/main/MyBoard.java
+++ MyBoard/src/main/MyBoard.java
@@ -10,19 +10,26 @@ public class MyBoard {
 
 	private Map<User, ArrayList<BoardNonBlind>> list = new HashMap<>();
 	private Set<BoardBlind> unknownList = new HashSet<>();
-	
-	
+
 	public MyBoard() {
-		
+
 	}
-	
+
 	public void run() {
 
 	}
 
 	/* print */
+	//print start Menu 
 	private void printStart() {
 		System.out.println("1)회원가입");
 		System.out.println("2)로그인");
+		System.out.println("3)게시글보기");
+	}
+	
+	//print Admin Menu
+	private void printAdmin() {
+		System.out.println("1)유저관리");
+		System.out.println("2)게시글관리");
 	}
 }
