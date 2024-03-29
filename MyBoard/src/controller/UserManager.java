2fb4bf7d5f0c1879400bc4c6176a1a1c2873058c
seungjae <juaje03@naver.com>
2024-03-29 11:34:55 +0900

    Add: SignUp Method-> signUp, addUser Method in MyBoard, UserManager Class

--- MyBoard/src/controller/UserManager.java
+++ MyBoard/src/controller/UserManager.java
@@ -9,10 +9,23 @@ public class UserManager {
 
 	}
 
-	private static ArrayList<User> userList = new ArrayList<>();
+	private ArrayList<User> userList = new ArrayList<>();
 	private static UserManager instance = new UserManager();
 
 	public static UserManager getInstance() {
 		return instance;
+	}
+
+	/* C(insert) */
+	public void addUser(User user) {
+		userList.add(user);
+	}
+
+	/* R(select) */
+	public String idCheck(String id) {
+		for (User user : userList)
+			if (user.getId().equals(id))
+				return "";
+		return id;
 	}
 }
--- MyBoard/src/main/MyBoard.java
+++ MyBoard/src/main/MyBoard.java
@@ -39,9 +39,16 @@ public class MyBoard {
 
 	public void run() {
 		while (true) {
+			printCheck();
 			printStart();
 			int sel = inputNum("메뉴입력");
+			Start(sel);
 		}
+	}
+
+	/* check */
+	private void printCheck() {
+		System.out.println(list);
 	}
 
 	/* print */
@@ -65,12 +72,69 @@ public class MyBoard {
 		System.out.println("3)게시글작성");
 		System.out.println("4)마이페이지");
 	}
-	
-	//print userSubmenu(4.마이페이지)
+
+	// print userSubmenu(4.마이페이지)
 	private void printUserSubMenu() {
 		System.out.println("1)회원정보");
 		System.out.println("2)게시글 관리");
 	}
+
+	/* menu Mehthod */
+	// start menu
+	private void Start(int sel) {
+		switch (sel) {
+		case 1:
+			signUp();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		}
+	}
+
+	// userMenu
+	private void User(int sel) {
+		switch (sel) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			break;
+		}
+	}
+
+	/* start method */
+	//sighUp Method
+	private void signUp() {
+		String id = inputString("ID");
+		id = userManager.idCheck(id);
+		if (id.equals("")) {
+			System.err.println("있는아이디입니다.");
+			return;
+		}
+		
+		String name = inputString("name");
+		String pw = inputString("pw");
+		String pwCheck = inputString("pwCheck");
+		if (!pw.equals(pwCheck)) {
+			System.err.println("비밀번호가 일치하지 않습니다.");
+			return;
+		}
+		addUser(id, pwCheck, name);
+	}
 	
-	
+	//addUser Method
+	private void addUser(String id, String pw, String name) {
+		User user = new User(id, pw, name);
+		userManager.addUser(user); //UserManager class-> add of userList
+		list.put(user, new ArrayList<BoardNonBlind>()); // add of list
+	}
 }
